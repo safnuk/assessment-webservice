98ad5baf80dbd36057071f8af7515039bf956952
Brad Safnuk <safnuk@gmail.com>
2017-12-11 13:16:21 -0500

    Add comments

--- src/Assessment/Config.hs
+++ src/Assessment/Config.hs
@@ -1,3 +1,6 @@
+-- Do not change ANYTHING between the horizontal lines.
+-- You have been warned!
+-- -------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 
 module Assessment.Config where
@@ -14,13 +17,25 @@ javaClass :: String
 compileCmd :: String
 runCmd :: String
 
+-- -------------------------------------------------
+-- Change these values as needed, but preserve the formatting.
+
+-- Access port for the logging service 
 logPort = 3001
+-- Access port for the assignment service
 testsPort = 3001
+-- URL for the logging service
 logURL = http "localhost" /: "log" /: "assessment"
+-- URL for the assignment service
 testsURL = http "localhost" /: "assignment" /: "tests"
+
+-- Maximum number of requests by a single user in the specified amount of time
 maxRequestFrequency = 2
 requestCounterTimeout = 60
 
+-- Java class name for all code samples submitted to the service
 javaClass = "Test"
+-- Program used to compile code samples
 compileCmd = "javac"
+-- Program used to run the compiled binary
 runCmd = "java"
